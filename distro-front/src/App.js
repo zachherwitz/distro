b6de728b29aa3307c31857fc5bb9ea216dfd297d
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-12 07:42:12 -0400

    oops was working in master but now theres a front end test to show the call sheet chanes

--- distro-front/src/App.js
+++ distro-front/src/App.js
@@ -1,91 +1,53 @@
 import React from 'react';
+import UserTestForm from './components/UserTestForm';
 import axios from 'axios';
+
+// TEST <UserTestForm /> to test the users
 
 class App extends React.Component {
   state = {
-    distroResults: [],
-    user: {
-      callsheet: {
-        callTime: null,
-        location: ""
-      },
-      department: "",
-      distros: [],
-      email: "",
-      name: "",
-      phone: "",
-      projectId: "",
-      role: ""
-    }
+    users: [],
+    displayUser: ''
   }
 
-  distroSearch = (e) => {
-    e.preventDefault()
+  componentDidMount = () => {
     axios.get('http://localhost:3000/users').then((response) => {
-      let results = []
-      console.log(response.data);
-      response.data.map((user, index) => {
-        user.distros.map((distro, index) => {
-          if (distro === this.distroName.value) {
-            results.push(user.name)
-          }
-        })
-      })
       this.setState({
-        distroResults:results
+        users: response.data
       })
     })
   }
 
-  nameSearch = (e) => {
-    e.preventDefault();
-    axios.get('http://localhost:3000/users').then((response) => {
-      console.log(response.data);
-      response.data.map((user, index) => {
-        if (user.name === this.searchName.value) {
-          console.log('Its a match!');
-          this.setState({
-            user: response.data[index]
-          })
-        }
-      })
+  displayUserProfile = (e) => {
+    let userToDisplay = this.state.users[e.target.id]
+    this.setState({
+      displayUser:userToDisplay
     })
   }
 
   render = () => {
-    const {user} = this.state;
-    const {distroResults} = this.state;
-    return <div className="container">
-      <form onSubmit={this.nameSearch}>
-        <input ref={input => this.searchName = input} type="text" placeholder="enter name"/>
-        <input type="submit" value="search for name" />
-      </form>
-      <form onSubmit={this.distroSearch}>
-        <input ref={input => this.distroName = input} type="text" placeholder="enter distros"/>
-        <input type="submit" value="search for distros" />
-      </form>
-      <div>
-        {distroResults[0] ?
-        <ul>
-          {distroResults.map((distroResult, index) => {
-            return <li key={index}>{distroResult}</li>
-          })}
-        </ul> : null}
-      </div>
-      <div>
-        {user.name ?
+    const {displayUser} = this.state;
+    return <div>
+      <ul> Users:
+        {this.state.users.map((user, index) => {
+          return <li key={index} id={index} onClick={this.displayUserProfile}>{user.name}</li>
+        })}
+      </ul>
+      <br/><br/>
+      <br/><br/>
+      <br/><br/>
+      {displayUser ?
         <div>
-          <h1>{user.name}</h1> <br/>
-          <p>{user.department}</p> <br/><br/>
-          <p>{user.callsheet.callTime ? "Call Time: " + user.callsheet.callTime : null }</p><br/>
-          <p>{user.callsheet.location ? "Location: " + user.callsheet.location : null}</p><br/><br/>
-          <ul> Distro Lists:
-            {user.distros.map((distro, index) => {
-              return <li key={index}>{distro}</li>
-            })}
-          </ul>
-        </div> : null }
-      </div>
+          <h1>NAME: {displayUser.name}</h1>
+          <h2>CALL TIME: {displayUser.callsheet.callTime} </h2>
+          <h2>LOCATION: {displayUser.callsheet.location}</h2>
+          <h3>DEPARTMENT: {displayUser.email}</h3>
+          <h4>EMAIL: {displayUser.email}</h4>
+          <h4>PHONE: {displayUser.email}</h4>
+          <h6>id: {displayUser._id}</h6>
+        </div>
+        : null
+      }
     </div>
   }
 }
