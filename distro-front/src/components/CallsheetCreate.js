0005cee3071049f97225dfc786098e938c5d8318
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-12 12:26:04 -0400

    create callsheet frontend first draft

--- distro-front/src/components/CallsheetCreate.js
+++ distro-front/src/components/CallsheetCreate.js
@@ -1,9 +1,106 @@
 import React from 'react';
 
 class CallsheetCreate extends React.Component {
+  state = {
+
+  }
+
+  composeCallsheet = () => {
+    let callsheetObject = {
+      date: this.state.date,
+      episode: this.state.episode,
+      day: this.state.day,
+      scriptDraft: this.state.scriptDraft,
+      generalCallTime: this.state.crewCallTime,
+      generalLocation: this.state.crewLocation,
+      nearestHospital: this.state.hospital
+    }
+    this.props.createCallsheet(callsheetObject);
+  }
+
+  newInput = (e) => {
+    const attributeId = e.target.getAttribute('id');
+    switch (attributeId) {
+      case 'date':
+        // console.log('changing date');
+        this.setState({[attributeId]:e.target.value})
+        break;
+      case 'episode':
+        // console.log('changing episode');
+        this.setState({[attributeId]:e.target.value})
+        break;
+      case 'day':
+        // console.log('changing day');
+        this.setState({[attributeId]:e.target.value})
+        break;
+      case 'scriptDraft':
+        // console.log('changing script draft');
+        this.setState({[attributeId]:e.target.value})
+        break;
+      case 'crewCallTime':
+        // console.log('changing crew call time');
+        this.setState({[attributeId]:e.target.value})
+        break;
+      case 'crewLocation':
+        // console.log('changing crew location');
+        this.setState({[attributeId]:e.target.value})
+        break;
+      case 'hospital':
+        // console.log('changing hospital');
+        this.setState({[attributeId]:e.target.value})
+        break;
+    }
+  }
+
   render = () => {
-    return <h1>This will eventually be a place to create Callsheets</h1>
+    return <div>
+      <form onSubmit={this.composeCallsheet}>
+        <input
+          onKeyDown={this.newInput}
+          id="date"
+          type="text"
+          placeholder="date"/>
+        <br/>
+        <input
+          onKeyDown={this.newInput}
+          id="episode"
+          type="text"
+          placeholder="episode"/>
+        <br/>
+        <input
+          onKeyDown={this.newInput}
+          id="day"
+          type="text"
+          placeholder="day"/>
+        <br/>
+        <input
+          onKeyDown={this.newInput}
+          id="scriptDraft"
+          type="text"
+          placeholder="script draft"/>
+        <br/>
+        <input
+          onKeyDown={this.newInput}
+          id="crewCallTime"
+          type="text"
+          placeholder="crew call time"/>
+        <br/>
+        <input
+          onKeyDown={this.newInput}
+          id="crewLocation"
+          type="text"
+          placeholder="crew location"/>
+        <br/>
+        <input
+          onKeyDown={this.newInput}
+          id="hospital"
+          type="text"
+          placeholder="nearest hopsital"/>
+        <br/>
+        <input type="submit" value="Submit Callsheet"/>
+      </form>
+    </div>
   }
 }
 
-export default CallsheetCreate 
+export default CallsheetCreate
